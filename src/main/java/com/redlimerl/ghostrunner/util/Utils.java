55a78b031c62f3e72627a388ddc6853dca351b8e
RedLime <redlimerl@gmail.com>
2021-11-05 15:19:14 +0900

    Change thread

--- src/main/java/com/redlimerl/ghostrunner/util/Utils.java
+++ src/main/java/com/redlimerl/ghostrunner/util/Utils.java
@@ -33,8 +33,8 @@ public class Utils {
     }
 
     public static void downloadPlayerSkin(UUID uuid) {
-        try {
-            new Thread(() -> {
+        new Thread(() -> {
+            try {
                 MinecraftSessionService sessionService = MinecraftClient.getInstance().getSessionService();
                 PlayerSkinProvider skinProvider = MinecraftClient.getInstance().getSkinProvider();
 
@@ -48,10 +48,10 @@ public class Utils {
                         ReplayGhost.addPlayerSkin(uuid, skin);
                     }
                 }
-            }).start();
-        } catch (Exception e) {
-            GhostRunner.debug("Failed load ghost's skin.");
-        }
+            } catch (Exception e) {
+                GhostRunner.debug("Failed load ghost's skin.");
+            }
+        }).start();
     }
 
     public static boolean isUrl(String text) {
